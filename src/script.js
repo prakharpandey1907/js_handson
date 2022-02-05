f0f0359e7314785c9da328a8114943bdd1765193
cedcoss <prakharpandey@cedcoss.com>
2022-02-05 13:31:43 +0530

    Stand in Line

--- src/script.js
+++ src/script.js
@@ -1,9 +1,16 @@
-// Setup
-let processed = 0;
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item);
+  let a=arr.shift(0);
 
-function processArg(num) {
-  return (num + 3) / 5;
+  return a;
+  // Only change code above this line
 }
 
-// Only change code below this line
- processed=processArg(7);
+// Setup
+const testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));

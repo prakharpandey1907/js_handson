460fbd747ebc52e11361ffa631190d6ea2dc0e44
cedcoss <prakharpandey@cedcoss.com>
2022-02-07 11:19:53 +0530

    replacing-if-else-chains-with-switch

--- src/script.js
+++ src/script.js
@@ -1,25 +1,25 @@
-function sequentialSizes(val) {
+function chainToSwitch(val) {
   var answer = "";
   // Only change code below this line
   switch (val) {
-    case 1:
-    case 2:
-    case 3:
-      return "Low";
+    case "bob":
+      answer = "Marley";
       break;
-    case 4:
-    case 5:
-    case 6:
-      return "Mid";
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
       break;
     case 7:
-    case 8:
-    case 9:
-      return "High";
+      answer = "Ate Nine";
       break;
   }
   // Only change code above this line
   return answer;
 }
 // Change this value to test
-sequentialSizes(1);
+chainToSwitch(7);

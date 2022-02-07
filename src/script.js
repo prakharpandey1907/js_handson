ed984e1fac9c9608834176c2c1b746e4ea056a71
cedcoss <prakharpandey@cedcoss.com>
2022-02-07 11:46:05 +0530

    iterate-with-javascript-for-loops

--- src/script.js
+++ src/script.js
@@ -1,9 +1,13 @@
 // Setup
-const myArray = [];
+
 
 // Only change code below this line
-var i = 5;
-while(i >= 0) {
+var ourArray = [];
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+var myArray = [];
+for (var i = 1; i < 6; i++) {
   myArray.push(i);
-  i--;
 }

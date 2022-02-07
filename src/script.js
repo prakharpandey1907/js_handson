b939c4171f87fd5a8bdc6dfaaf5820cd9dc9b155
cedcoss <prakharpandey@cedcoss.com>
2022-02-07 11:31:45 +0530

    delete-properties-from-a-javascript-object

--- src/script.js
+++ src/script.js
@@ -1,9 +1,11 @@
-var myDog = {
-  name: "Happy Coder",
-  legs: 4,
-  tails: 1,
-  friends: ["freeCodeCamp Campers"]
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
 };
 
-// Only change code below this line.
-myDog.bark = "woof";
+// Only change code below this line
+delete myDog.tails;

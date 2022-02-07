303417167606ff6a30f965b4a2751d9d07d0e0fa
cedcoss <prakharpandey@cedcoss.com>
2022-02-07 11:42:27 +0530

    accessing-nested-objects

--- src/script.js
+++ src/script.js
@@ -1,16 +1,13 @@
-var myMusic = [
-  {
-    artist: "Billy Joel",
-    title: "Piano Man",
-    release_year: 1973,
-    formats: ["CD", "8T", "LP"],
-    gold: true
-  },
-  // Add record here
-  {
-    artist: "Deep Purple",
-    title: "Smoke on the water",
-    release_year: 1976,
-    formats: ["CD", "8T", "LP"]
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
   }
-];
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]

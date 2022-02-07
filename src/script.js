dfac546ca177380de6d74a605f0c2e2030cbceb8
cedcoss <prakharpandey@cedcoss.com>
2022-02-07 11:48:18 +0530

    count-backwards-with-a-for-loop

--- src/script.js
+++ src/script.js
@@ -1,7 +1,13 @@
-// Setup
-const myArray = [];
+var ourArray = [];
 
-// Only change code below this line
-for (var i = 1; i < 10; i += 2) {
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 9; i > 0; i -= 2) {
   myArray.push(i);
 }

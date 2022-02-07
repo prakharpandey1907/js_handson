b4302f11384891c3b44b736c9017b9e2b5729d89
cedcoss <prakharpandey@cedcoss.com>
2022-02-07 11:26:28 +0530

    counting-card

--- src/script.js
+++ src/script.js
@@ -1,29 +1,10 @@
-let count = 0;
-
-function cc(card) {
+const myDog = {
   // Only change code below this line
-  switch (card) {
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      count++;
-      break;
-    case 10:
-    case "J":
-    case "Q":
-    case "K":
-    case "A":
-      count--;
-      break;
-  }
-  if (count > 0) {
-    return count + " Bet";
-  } else {
-    return count + " Hold";
-  }
-  // Only change code above this line
-}
+"name": "Whiskers",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Water", "Dogs"]
 
-cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+  // Only change code above this line
+};

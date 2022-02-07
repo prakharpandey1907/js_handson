0914d93a502630c1b06eeacdd6f09c47cd25f0ae
cedcoss <prakharpandey@cedcoss.com>
2022-02-07 11:58:35 +0530

    generate-random-whole-numbers-within-a-range

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
-function randomWholeNum() {
-
+function randomRange(myMin, myMax) {
   // Only change code below this line
+ 
+  return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
 
-  return Math.floor(Math.random() * 10);
+  // Only change code above this line
 }

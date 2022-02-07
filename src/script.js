667e7a715d7c2bce9e98e679b21a0cbed4e3444e
cedcoss <prakharpandey@cedcoss.com>
2022-02-07 11:50:06 +0530

    nesting-for-loops

--- src/script.js
+++ src/script.js
@@ -1,8 +1,14 @@
-// Setup
-const myArr = [2, 3, 4, 5, 6];
-
-// Only change code below this line
-let total = 0;
-for (let i = 0; i < myArr.length; i++) {
-  total += myArr[i];
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      product = product * arr[i][j];
+    }
+  }
+  // Only change code above this line
+  return product;
 }
+
+// Modify values below to test your code
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
